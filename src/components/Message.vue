b4ed22a5397731bcafbe28c1ea2343f722f85a01
mgthomas99 <m.g.thomas99@gmail.com>
2018-05-16 18:57:37 +0100

    replaced "Hello" components with message component

---
+++ src/components/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="message-component">
+    <div class="content">
+      <span>The message is {{ content }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from "vue-property-decorator";
+
+  /**
+   * A simple component for displaying a message.
+   */
+  @Component({ name: "message" })
+  export default class MessageComponent
+  extends Vue {
+
+    /**
+     * The message to be displayed by this component. If none is specified, a
+     * default message will be displayed.
+     */
+    @Prop()
+    public content: string
+        = "Hello, world!";
+
+    public constructor() {
+      super();
+    }
+
+  }
+</script>
+
+<style lang="css">
+  #hello-decorator-component {
+    font-size: 2em;
+  }
+</style>
